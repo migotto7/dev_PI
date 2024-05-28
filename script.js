2c3e521ab867c40d845db8527b8872a663309d73
elthon-migotto <elthon.migotto@specchio.info>
2024-05-28 16:29:30 -0300

    css updates

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var markersId = {};
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    maxZoom: 18,
+    maxZoom: 10,
+    minZoom: 2,
 }).addTo(map);
 
 var markers = [
